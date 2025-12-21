22189702dd575af08bdfd28ce8706500fecffa83
26LiveKnife26 <photoboxshop4@gmail.com>
2025-12-21 21:51:26 +1000

    добавлены куки и базовое приветствие

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-import curses
+from tools import center
+import simple_term_menu
+import art
+import json
+
+with open('backend/cookies.json', 'r') as cookies:
+    cookie = json.load(cookies)
+    
+art.tprint("*   LPT   *")
+if cookie["user"]["first?"] == "yes":
+    print("вы впервые, не хотели ли бы вы прочитать инструкцию?")
+    choice_index = simple_term_menu.TerminalMenu(
+        ["да", "нет"],
+        menu_cursor="→ ",
+        clear_screen=False
+    ).show()
+    
+    if choice_index == 0:
+        print("")
+        input("\nнажмите Enter для продолжения...")
+        
+    cookie["user"]["first?"] = "no"
+    with open('backend/cookies.json', 'w') as cookies:
+        json.dump(cookie, cookies, indent=4)
+        
+print("добро пожаловать в LPT!")
